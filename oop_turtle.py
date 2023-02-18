507e6208af704721347090f38de8d3c3c31ca671
rwestrope <rewestrope@gmail.com>
2023-02-18 12:07:02 -0600

    finished up course

--- oop_turtle.py
+++
@@ -1,9 +0,0 @@
-import turtle
-
-my_turtle = turtle.Turtle()
-my_turtle.shape("turtle")
-my_turtle.color("red")
-
-my_turtle.forward(100)
-
-turtle.done()
--- snake_game.py
+++ snake_game.py
@@ -6,7 +6,7 @@ import random
 WIDTH = 500
 HEIGHT = 500
 DELAY = 100
-FOOD_SIZE = 10
+FOOD_SIZE = 15
 
 offsets = {
     "up": (0, 20),
@@ -15,25 +15,27 @@ offsets = {
     "right": (20, 0)
 }
 
-def go_up():
-    global snake_direction
-    if snake_direction != "down":
-        snake_direction = "up"
+def bind_direction_keys():
+    screen.onkey(lambda: set_snake_direction("up"), "Up")
+    screen.onkey(lambda: set_snake_direction("down"), "Down")
+    screen.onkey(lambda: set_snake_direction("right"), "Right")
+    screen.onkey(lambda: set_snake_direction("left"), "Left")
 
-def go_down():
+def set_snake_direction(direction):
     global snake_direction
-    if snake_direction != "up":
-        snake_direction = "down"
+    if direction == "up":
+        if snake_direction != "down":
+            snake_direction = "up"   
+    elif direction == "down":
+        if snake_direction != "up":
+            snake_direction = "down"
+    elif direction == "right":
+        if snake_direction != "left":
+            snake_direction = "right"
+    elif direction == "left":
+        if snake_direction != "right":
+            snake_direction = "left"
 
-def go_right():
-    global snake_direction
-    if snake_direction != "left":
-        snake_direction = "right"
-
-def go_left():
-    global snake_direction
-    if snake_direction != "right":
-        snake_direction = "left"
 
 def game_loop():
     stamper.clearstamps()
@@ -44,8 +46,8 @@ def game_loop():
 
     #check collisions
     if new_head in snake or new_head[0] < -WIDTH/2 or new_head[0] > WIDTH/2\
-        or new_head[1] < -HEIGHT/2 or new_head[1] > HEIGHT/2:
-        turtle.bye()
+            or new_head[1] < -HEIGHT/2 or new_head[1] > HEIGHT/2:
+        reset()
     else:
 
         snake.append(new_head)
@@ -53,11 +55,11 @@ def game_loop():
         if not food_collision():
             snake.pop(0) #snake stays same length if no food collision 
 
-        snake.pop(0)
-
         for segment in snake:
             stamper.goto(segment[0], segment[1])
             stamper.stamp()
+
+        screen.title(f"Snake Game. Score: {score}")
 
         screen.update()
 
@@ -70,9 +72,10 @@ def get_distance(pos1, pos2):
     return distance
 
 def food_collision():
-    global food_pos
+    global food_pos, score
     if get_distance(snake[-1], food_pos) < 20:
-        food_pos = get_random_food_pos
+        score += 1
+        food_pos = get_random_food_pos()
         food.goto(food_pos)
         return True
     return False
@@ -82,44 +85,41 @@ def get_random_food_pos():
     y = random.randint(-HEIGHT/2 + FOOD_SIZE, HEIGHT/2 - FOOD_SIZE)
     return (x, y)
 
+def reset():
+    global score, snake, snake_direction, food_pos
+    score = 0
+    snake = [[0,0], [20,0], [40,0], [60,0]]
+    snake_direction = "up"
+
+    food_pos = get_random_food_pos()
+    food.goto(food_pos)
+
+    game_loop()
+
 # Create a window where we will do our drawing.
 screen = turtle.Screen()
 screen.setup(WIDTH, HEIGHT)  # Set the dimensions of the Turtle Graphics window.
 screen.title("Snake")
-screen.bgcolor("cyan")
+screen.bgcolor("black")
 screen.tracer(0)
 
 #event handler
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_right, "Right")
-screen.onkey(go_down, "Down")
-screen.onkey(go_left, "Left")
+bind_direction_keys()
 
 # Create a turtle to do your bidding
 stamper = turtle.Turtle()
-stamper.shape("square")
+stamper.shape("circle")
+stamper.color("green")
 stamper.penup()
 
-#Create snake as coordinate pairs
-snake = [[0,0], [20,0], [40,0], [60,0]]
-snake_direction = "up"
-
-
-#draw snake
-for segment in snake:
-    stamper.goto(segment[0], segment[1])
-    stamper.stamp()
-
 food = turtle.Turtle()
-food.shape("circle")
+food.shape("triangle")
 food.color("red")
 food.shapesize(FOOD_SIZE/20)
 food.penup()
-food_pos = get_random_food_pos()
-food.goto(food_pos)
 
-game_loop()
+reset()
 
 # This statement (or an equivalent) is needed at the end of all your turtle programs.
 turtle.done()
